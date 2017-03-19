64f5cbe74c1f68ff587e9135a3601b859cc1ff11
Robert Gevorgyan <robert1999.g@gmail.com>
2017-03-19 19:34:34 +0400

    Complete styling and layout

--- lib/todo.jsx
+++ lib/todo.jsx
@@ -11,6 +11,51 @@ class CurrentTodo extends React.Component {
   render() {
     return (
       <div className='todo'>
+        <div className='todo-name'>
+          <h1>Work</h1>
+          <p>1 of 5 Done</p>
+        </div>
+        <div className='todos'>
+          <li>
+            <div className="checkbox">
+              <input type="checkbox" value="None" id="checkbox1" name="check" />
+              <label htmlFor="checkbox1"></label>
+            </div>
+            Ask for a raise
+          </li>
+          <li>
+            <div className="checkbox">
+              <input type="checkbox" value="None" id="checkbox2" name="check" />
+              <label htmlFor="checkbox2"></label>
+            </div>
+            Scream at my co-workers
+          </li>
+          <li>
+            <div className="checkbox">
+              <input type="checkbox" value="None" id="checkbox3" name="check" />
+              <label htmlFor="checkbox3"></label>
+            </div>
+            Send out the recent paperwork
+          </li>
+          <li>
+            <div className="checkbox">
+              <input type="checkbox" value="None" id="checkbox4" name="check" />
+              <label htmlFor="checkbox4"></label>
+            </div>
+            Buy a present for my Boss
+          </li>
+          <li className='done'>
+            <div className="checkbox">
+              <input type="checkbox" value="None" id="checkbox5" name="check" />
+              <label htmlFor="checkbox5"></label>
+            </div>
+            Watch some funny videos on youtube when nobody is looking
+          </li>
+        </div>
+        <div className='add-todo'>
+          <input type='text' className='input' placeholder='Enter new Todo' />
+          <div className='add'>+</div>
+        </div>
       </div>
     );
   }
--- lib/todolists.jsx
+++ lib/todolists.jsx
@@ -8,24 +8,24 @@ class TodoLists extends React.Component {
     super();
   }
 
-  /*
-  <h4>Todo Lists</h4>
-  <input type='text' placeholder='Search for the List' />
-  <div class='todos'>
-    <li>Private</li>
-    <li>Work</li>
-    <li>Training</li>
-    <li>Friends</li>
-  </div>
-  <div>
-    <input type='text' placeholder='Search for the List' />
-    <div className='add'>+</div>
-  </div>
-  */
-
   render() {
     return (
       <div className='todo-list'>
+        <h4>Todo Lists</h4>
+        <div className='search-wrapper'>
+          <i className="fa fa-search" aria-hidden="true"></i>
+          <input type='text' className='search' placeholder='Search for List' />
+        </div>
+        <div className='todos'>
+          <li>Private</li>
+          <li className='active'>Work</li>
+          <li>Training</li>
+          <li>Friends</li>
+        </div>
+        <div className='add-todo'>
+          <input type='text' className='input' placeholder='Enter new list name' />
+          <div className='add'>+</div>
+        </div>
       </div>
     );
   }
